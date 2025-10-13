cb080e46bc1c58616c26529ed9f1c70c03c15804
Hamza Mahmood <724532@student.inholland.nl>
2025-10-13 18:16:06 +0200

    Added Changes to login , Made employee login as role add a nav bar

--- NoSQL_Project/Controllers/EmployeesController.cs
+++ NoSQL_Project/Controllers/EmployeesController.cs
@@ -50,6 +50,8 @@ namespace NoSQL_Project.Controllers
 				ModelState.AddModelError(string.Empty, "Invalid email or password.");
 				return View(loginModel);
 			}
+			
+
 
 
 			// ✅ Store in session
@@ -57,7 +59,19 @@ namespace NoSQL_Project.Controllers
 			HttpContext.Session.SetString("EmployeeName", employee.FirstName);
 			HttpContext.Session.SetString("EmployeeRole", employee.UserRole.ToString());
 
-            return RedirectToAction("Index", "Employees");
+			switch (employee.UserRole.ToString().ToLower())
+			{
+				case "employee":
+					return RedirectToAction("Index", "Tickets"); 
+				case "service_desk_employee ":
+					return RedirectToAction("Index", "Employees"); 
+				case "manager":
+					return RedirectToAction("Index", "Employees"); 
+				default:
+					return RedirectToAction("Index", "Home"); 
+
+			}
+                 
 
 
 
@@ -66,7 +80,7 @@ namespace NoSQL_Project.Controllers
 		public IActionResult Logout()
 		{
 			HttpContext.Session.Clear();
-			return RedirectToAction("Login");
+			return RedirectToAction("Login", "Employees");
 		}
 
 		[HttpGet]
