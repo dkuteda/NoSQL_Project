b1d1f44a75b7872fdc3ae9a2a61806cb1be2ef82
dkuteda <728353@student.inholland.nl>
2025-11-06 16:30:39 +0100

    push

---
+++ NoSQL_Project/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoSQL_Project.ViewModels;
+
+namespace NoSQL_Project.Controllers
+{
+	public class AccountController : Controller
+	{
+		public async Task<IActionResult> ForgotPassword()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> ForgotPassword(ForgotPasswordViewModel model) 
+		{
+			return View();
+		}
+
+		public async Task<ActionResult> ForgotPasswordConfirmation()
+	}
+}
